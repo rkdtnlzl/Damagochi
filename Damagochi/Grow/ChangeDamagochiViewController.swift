140635c72a1473f13e30c788aeb4e8fbcd310854
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-09 22:45:42 +0900

    [Add] 다마고치 변경 vc 생성

---
+++ Damagochi/Grow/ChangeDamagochiViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChangeDamagochiViewController.swift
+//  Damagochi
+//
+//  Created by 강석호 on 6/9/24.
+//
+
+import UIKit
+
+class ChangeDamagochiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "다마고치 변경하기"
+        
+        configureHierarchy()
+        configureUI()
+        configureLayout()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureUI() {
+        
+        view.backgroundColor = UIColors.mainBackgroundColor
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+
+}
