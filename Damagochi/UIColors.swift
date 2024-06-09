a8826c613fc1b6a4aa9d50e959bc84e23a99e119
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-09 22:44:32 +0900

    [Feat] UIColor 색 추가

---
+++ Damagochi/UIColors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColors.swift
+//  Damagochi
+//
+//  Created by 강석호 on 6/7/24.
+//
+
+import Foundation
+import UIKit
+
+enum UIColors {
+    
+    
+    static let mainBackgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    static let mainFontColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
